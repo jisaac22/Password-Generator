78b2bd7f614520e019c8245b45062e691061cf02
Jacob Sierra <jacobisaac22@yahoo.com>
2021-05-10 21:57:31 -0500

    added a prompt with questions

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var symbols = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+var choices = ["y", "n"];
 
 console.log(lowerCase);
 console.log(upperCase);
@@ -17,13 +18,15 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
+//added a couple prompt questions 
 function options(){
   var optionSelectors= confirm("Please select from the following");
+  optionSelectors= prompt("Do you want symbols?")
 }
