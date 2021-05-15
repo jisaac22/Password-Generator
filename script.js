0ee4d7e42f5a95fc5be472067058623e6eded8f8
Jacob Sierra <jacobisaac22@yahoo.com>
2021-05-14 23:38:04 -0500

    switched my variables, wrong variables were selected

--- script.js
+++ script.js
@@ -23,15 +23,15 @@ var userCHOICES;
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+    function writePassword() {
+    var password = generatePassword();
 
-  var passwordText = document.querySelector("#password");
+    var passwordText = document.querySelector("#password");
 
 
-  passwordText.value = password;
+   passwordText.value = password;
 
-}
+ }
 
 // added a couple prompt questions 
 function generatePassword(){
@@ -46,69 +46,86 @@ function generatePassword(){
         userchoiceSymbols= confirm ("Dp you want to include Symbols?");
       };
 // added functions if none are selected or if all are selected
-     if (!userchoiceUPPER && !userchoiceLOWER && !userchoiceNumbers && !userchoiceSymbols){
+     if (!upperCase && !lowerCase && !numbers && !symbols){
        userCHOICES = alert("Please select at least 1 option")
-     }else if (userchoiceUPPER && userchoiceLOWER && userchoiceNumbers && userchoiceSymbols){
-       userCHOICES = userchoiceUPPER.concat(userchoiceLOWER, userchoiceNumbers, userchoiceSymbols);
+     }
+     else if (upperCase && lowerCase && numbers && symbols){
+       userCHOICES = upperCase.concat(lowerCase, numbers, symbols);
        console.log(userCHOICES);
     }
 // added functions if only 3 choices are selected 
-    else if (userchoiceUPPER && userchoiceLOWER && userchoiceNumbers && !userchoiceSymbols){
-    userCHOICES = userchoiceUPPER.concat(userchoiceLOWER, userchoiceNumbers);
+    else if(upperCase && lowerCase && numbers && !symbols) {
+    userCHOICES = upperCase.concat(lowerCase, numbers);
     console.log(userCHOICES);
     }
-    else if (userchoiceUPPER && userchoiceLOWER && !userchoiceNumbers && userchoiceSymbols){
-      userCHOICES = userchoiceUPPER.concat(userchoiceLOWER, userchoiceSymbols);
+    else if (upperCase && lowerCase && !numbers && symbols){
+      userCHOICES = upperCase.concat(lowerCase, symbols);
     console.log(userCHOICES);
     }
-    else if (userchoiceUPPER && !userchoiceLOWER && userchoiceNumbers && userchoiceSymbols){
-      userCHOICES = userchoiceUPPER.concat(userchoiceNumbers, userchoiceSymbols)
+    else if (upperCase && !lowerCase && numbers && symbols){
+      userCHOICES = upperCase.concat(numbers, symbols);
     console.log(userCHOICES);
     }
-    else if  (!userchoiceUPPER && userchoiceLOWER && userchoiceNumbers && userchoiceSymbols){
-      userCHOICES = userchoiceLOWER.concat(userchoiceNumbers, userchoiceSymbols);
+    else if (!upperCase && lowerCase && numbers && symbols){
+      userCHOICES = lower.concat(numbers, symbols);
       console.log(userCHOICES);
     }   
 // added functions if only 2 are selected
-    else if (userchoiceUPPER && userchoiceLOWER && !userchoiceNumbers && !userchoiceSymbols){
-      userCHOICES = userchoiceUPPER.concat(userchoiceLOWER);
+    else if (upperCase && lowerCase && !numbers && !symbols){
+      userCHOICES = upperCase.concat(lower);
       console.log(userCHOICES);
     }
-    else if (userchoiceUPPER && !userchoiceLOWER && userchoiceNumbers && !userchoiceSymbols){
-      userCHOICES = userchoiceUPPER.concat(userchoiceNumbers);
+    else if (upperCase && !lowerCase && numbers && !symbols){
+      userCHOICES = upperCase.concat(numbers);
       console.log(userCHOICES);
     }
-    else if (userchoiceUPPER && !userchoiceLOWER && !userchoiceNumbers && userchoiceSymbols){
-      userCHOICES - userchoiceSymbols.concat(userchoiceSymbols);
+    else if (!upperCase && !lowerCase && numbers && symbols){
+      userCHOICES - numbers.concat(symbols);
       console.log(userCHOICES);
     }
-    else if (!userchoiceUPPER && userchoiceLOWER && userchoiceNumbers && !userchoiceSymbols){
-      userCHOICES = userchoiceLOWER.concat(userchoiceNumbers);
+    else if (!upperCase && lowerCase && numbers && !symbols){
+      userCHOICES = lowerCase.concat(numbers);
       console.log(userCHOICES);
     }
-    else if (!userchoiceUPPER && userchoiceLOWER && !userchoiceNumbers && userchoiceSymbols){
-      userCHOICES = userchoiceLOWER.concat(userchoiceSymbols);
+    else if (!upperCase && lowerCase && !numbers && symbols){
+      userCHOICES = lowerCase.concat(symbols);
       console.log(userCHOICES);
     }
-    else if (!userchoiceUPPER && !userchoiceLOWER && userchoiceNumbers && userchoiceSymbols){
-      userCHOICES = userchoiceNumbers.concat(userchoiceSymbols);
+    else if (upperCase && !lowerCase && !numbers && symbols){
+      userCHOICES = upperCase.concat(symbols);
       console.log(userCHOICES);
     }
 // added functions for only 1 selected
-    else if (userchoiceUPPER && !userchoiceLOWER && !userchoiceNumbers && !userchoiceSymbols){
-      userCHOICES = userchoiceUPPER;
+    else if (upperCase && !lowerCase && !numbers && !symbols){
+      userCHOICES = upperCase;
       console.log (userCHOICES);
     }
-    else if (!userchoiceUPPER && userchoiceLOWER && !userchoiceNumbers && !userchoiceSymbols){
-      userCHOICES = userchoiceLOWER;
+    else if (!upperCase && lowerCase && !numbers && !symbols){
+      userCHOICES = lowerCase;
       console.log(userCHOICES)
     }
-    else if (!userchoiceUPPER && !userchoiceLOWER && userchoiceNumbers && !userchoiceSymbols){
-      userCHOICES = userchoiceNumbers;
+    else if (!upperCase && !lowerCase && numbers && !symbols){
+      userCHOICES = numbers;
       console.log(userCHOICES);
     }
-    else if (!userchoiceUPPER && !userchoiceLOWER && !userchoiceNumbers && userchoiceSymbols){
-      userCHOICES = userchoiceSymbols;
+    else if (!upperCase && !lowerCase && !numbers && symbols){
+      userCHOICES = symbols;
       console.log(userCHOICES)
- 
+    };
+  
+
+  var blankpassword = [];
+
+for (var i = 0; i < userChoiceLength; i++){
+  var allChoices = userCHOICES[Math.floor(Math.random() * userCHOICES.length)];
+  blankpassword.push(allChoices);
 }
+
+var password = blankpassword.join("");
+console.log("Here is your password " + password);
+return password;
+
+}
+  
+ 
+
